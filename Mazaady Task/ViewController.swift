c10579d0bf22ae3b054761f9284129b9d4031df1
diaasaeed <O-DIAA.SAIED@etisalat.com>
2025-04-10 19:30:27 +0200

    Initial Commit

---
+++ Mazaady Task/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Mazaady Task
+//
+//  Created by Diaa saeed on 10/04/2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
